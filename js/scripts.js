9b073e51e8f655ed05e9db44ac593350099af35d
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-10-28 10:06:02 +0200

    Skrev fler färger
    Ny background color när en ndefreader finns och om skanningen började.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,12 @@
 //Look if the device have NFC
 if ('NDEFReader' in window) {
     const ndef = new NDEFReader();
+    document.body.style.backgroundColor = "#ffff00"
         //Start scaning for NFC tags
 
         ndef.scan().then(() => {
         console.log("Scan started successfully.");
+        document.body.style.backgroundColor = "#A020F0";
         
         ndef.onreadingerror = (event) => {
             console.log("Error! Cannot read data from the NFC tag. Try a different one?");
