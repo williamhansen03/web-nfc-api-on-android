b785ecab078beddc1048ce9ab820a293c7ece45a
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-11-11 12:31:43 +0100

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-/*const color = document.body;
+const color = document.body;
 color.style.backgroundColor = "#000000";
 
 const text = document.querySelector("h1");
@@ -13,41 +13,36 @@ nfcText.innerHTML = nfcPermissionStatus;
 function startScanning(){
     const ndef = new NDEFReader();
 
-    while(true){
-
-        ndef.scan().then(() => {
-            text.innerHTML = "Scan started successfully.";
-            console.log("Scan started successfully.");
-            color.style.backgroundColor = "#A020F0";
+    ndef.scan().then(() => {
+        text.innerHTML = "Scan started successfully.";
+        console.log("Scan started successfully.");
+        color.style.backgroundColor = "#A020F0";
         
-            ndef.addEventListener("readingerror", () => {
-                text.innerHTML = "Error! Cannot read data from the NFC tag. Try a different one?";
-                console.log("Error! Cannot read data from the NFC tag. Try a different one?");
-                color.style.backgroundColor = "#ff0000";
-            });
-
-            ndef.addEventListener("reading", ({ message, serialNumber }) => {
-                const info = document.querySelector("h2");
-                info.innerHTML = message + ", " + serialNumber;
-                text.innerHTML = "NDEF message read.";
-                console.log("NDEF message read.");
-                delay(400).then(() => color.style.backgroundColor = "#00ff00");
-            });
-
-            }).catch((error) => {
-            text.innerHTML = `Error! Scan failed to start: ${error}.`;
-            console.log(`Error! Scan failed to start: ${error}.`);
+        ndef.addEventListener("readingerror", () => {
+            text.innerHTML = "Error! Cannot read data from the NFC tag. Try a different one?";
+            console.log("Error! Cannot read data from the NFC tag. Try a different one?");
             color.style.backgroundColor = "#ff0000";
         });
-    }
+
+        ndef.addEventListener("reading", ({ message, serialNumber }) => {
+            const info = document.querySelector("h2");
+            info.innerHTML = message + ", " + serialNumber;
+            text.innerHTML = "NDEF message read.";
+            console.log("NDEF message read.");
+            delay(400).then(() => color.style.backgroundColor = "#00ff00");
+        });
+
+        }).catch((error) => {
+        text.innerHTML = `Error! Scan failed to start: ${error}.`;
+        console.log(`Error! Scan failed to start: ${error}.`);
+        color.style.backgroundColor = "#ff0000";
+    });
+    
 }
 
 
 if ('NDEFReader' in window) {
     text.innerHTML = "Look if the device have NFC";
-    
-
-    
 
     color.style.backgroundColor = "#ffff00";
     
@@ -75,6 +70,6 @@ else{
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
-*/
-let j = document.querySelector("h1");
-j.innerHTML = "hej";
+
+//let j = document.querySelector("h1");
+//j.innerHTML = "hej";
