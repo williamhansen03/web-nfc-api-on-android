622e051f4c4851358c312c693c43c9c709979e63
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-11-11 10:34:00 +0100

    fix

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then((cache) => {
+            document.body.style.backgroundColor = "#BFFF00";
             return cache.addAll(["./", "manifest.json", "index.html", "js/scripts.js", "image/icon.png", "image/icon72x72.png", "image/icon144x144.png", "image/icon192x192.png"]);
         })
     );
@@ -10,6 +11,7 @@ self.addEventListener("install", e => {
 self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(response => {
+            document.body.style.backgroundColor = "#784212";
             return response || fetch(e.request);
         })
     );
