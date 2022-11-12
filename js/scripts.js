79ee0da37cdeaa2eb35f4c4dce889ba2fd77061d
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-11-12 17:29:39 +0100

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,6 @@ color.style.backgroundColor = "#000000";
 const text = document.querySelector("h1");
 const nfcText = document.querySelector("p");
 
-const nfcPermissionStatus = "hej"//navigator.permissions.query({ name: "nfc" });
-
-nfcText.innerHTML = nfcPermissionStatus;
-
-
 //Look if the device have NFC
 function startScanning(){
     const ndef = new NDEFReader();
@@ -45,8 +40,24 @@ if ('NDEFReader' in window) {
     text.innerHTML = "Look if the device have NFC";
 
     color.style.backgroundColor = "#ffff00";
-    
-    if (nfcPermissionStatus === "granted") {
+
+    navigator.permissions.query({name:'nfc'}).then((result) => {
+        if (result.state === 'granted') {
+          startScanning();
+        } else if (result.state === 'prompt') {
+            // Show a "scan" button.
+            document.querySelector("#scanButton").style.display = "block";
+            document.querySelector("#scanButton").onclick = event => {
+            // Prompt user to allow UA to send and receive info when they tap NFC devices.
+          
+            startScanning();
+          
+          
+        };
+        }
+      });
+      
+    /*if (nfcPermissionStatus === "granted") {
         // NFC access was previously granted, so we can start NFC scanning now.
         startScanning();
 
@@ -60,10 +71,10 @@ if ('NDEFReader' in window) {
           
           
         };
-      }
+      }*/
         //Start scaning for NFC tags
 
-        
+        nfcText.innerHTML = navigator.permissions.query({name:'nfc'});
 }
 else{
     text.innerHTML = "No nfc reader or browser does not support NDEFReader";
@@ -73,6 +84,3 @@ else{
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
-
-//let j = document.querySelector("h1");
-//j.innerHTML = "hej";
