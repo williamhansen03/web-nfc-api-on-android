219f6d806a081e49477b09e6d179a08006dac38a
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-11-12 18:07:07 +0100

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ const color = document.body;
 color.style.backgroundColor = "#000000";
 
 const text = document.querySelector("h1");
+const info = document.querySelector("h2");
 const nfcText = document.querySelector("p");
 
 //Look if the device have NFC
@@ -10,26 +11,21 @@ function startScanning(){
 
     ndef.scan().then(() => {
         text.innerHTML = "Scan started successfully.";
-        console.log("Scan started successfully.");
         color.style.backgroundColor = "#A020F0";
         
         ndef.addEventListener("readingerror", () => {
             text.innerHTML = "Error! Cannot read data from the NFC tag. Try a different one?";
-            console.log("Error! Cannot read data from the NFC tag. Try a different one?");
             color.style.backgroundColor = "#ff0000";
         });
 
         ndef.addEventListener("reading", ({ message, serialNumber }) => {
-            const info = document.querySelector("h2");
             info.innerHTML = message + ", " + serialNumber;
             text.innerHTML = "NDEF message read.";
-            console.log("NDEF message read.");
             delay(400).then(() => color.style.backgroundColor = "#00ff00");
         });
 
         }).catch((error) => {
         text.innerHTML = `Error! Scan failed to start: ${error}.`;
-        console.log(`Error! Scan failed to start: ${error}.`);
         color.style.backgroundColor = "#ff0000";
     });
     
@@ -40,41 +36,26 @@ if ('NDEFReader' in window) {
     text.innerHTML = "Look if the device have NFC";
 
     color.style.backgroundColor = "#ffff00";
-
+    while(ture){
     navigator.permissions.query({name:'nfc'}).then((result) => {
         if (result.state === 'granted') {
           startScanning();
         } else if (result.state === 'prompt') {
             // Show a "scan" button.
             document.querySelector("#scanButton").style.display = "block";
-            document.querySelector("#scanButton").onclick = event => {
+            ndef.addEventListener("onLoad", () =>{//document.querySelector("#scanButton").onclick = event => {
             // Prompt user to allow UA to send and receive info when they tap NFC devices.
           
             startScanning();
           
           
-        };
+        });
         }
       });
-      
-    /*if (nfcPermissionStatus === "granted") {
-        // NFC access was previously granted, so we can start NFC scanning now.
-        startScanning();
+    }
+    //Start scaning for NFC tags
 
-      } else {
-        // Show a "scan" button.
-        document.querySelector("#scanButton").style.display = "block";
-        document.querySelector("#scanButton").onclick = event => {
-          // Prompt user to allow UA to send and receive info when they tap NFC devices.
-          
-            startScanning();
-          
-          
-        };
-      }*/
-        //Start scaning for NFC tags
-
-        nfcText.innerHTML = navigator.permissions.query({name:'nfc'}).state;
+    nfcText.innerHTML = navigator.permissions.query({name:'nfc'}).state;
 }
 else{
     text.innerHTML = "No nfc reader or browser does not support NDEFReader";
