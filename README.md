0c4246706ad9df92931510402cfe9925a9ac6749
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-11-14 10:30:11 +0100

    kommentarer

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 [mdm web docs_ Web NFC API](https://developer.mozilla.org/en-US/docs/Web/API/Web_NFC_API)
 
 [web.dev. Interact with NFC devices on Chrome for Android](https://web.dev/nfc/)
+
+[mdm web docs_ WorkerNavigator.permissions](https://developer.mozilla.org/en-US/docs/Web/API/WorkerNavigator/permissions)
+
+[WHAT WEB CAN DO TODAY?](https://whatwebcando.today/permissions.html)
+
+[mdm web docs_ Permissions.query()](https://developer.mozilla.org/en-US/docs/Web/API/Permissions/query)
--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,34 @@
 const color = document.body;
-color.style.backgroundColor = "#000000";
 
 const text = document.querySelector("h1");
 const info = document.querySelector("h2");
-const nfcText = document.querySelector("p");
 
+// Kod ska till service worker 
+
+// Function scan after a nfc tag
 function startScanning(){
+    // Get refercens to nfc reader
     const ndef = new NDEFReader();
     color.style.backgroundColor = "#00FFFF";
 
+    // Start scaning for NFC tags
     ndef.scan().then(() => {
         text.innerHTML = "Scan started successfully.";
-        color.style.backgroundColor = "#A020F0";
         
+        // If you get a error while reading a tag
         ndef.addEventListener("readingerror", () => {
             text.innerHTML = "Error! Cannot read data from the NFC tag. Try a different one?";
             color.style.backgroundColor = "#ff0000";
             delay(400).then(() => startScanning());
         });
-
+        // If you reading a tag successful 
         ndef.addEventListener("reading", ({ message, serialNumber }) => {
             info.innerHTML = message + ", " + serialNumber;
             text.innerHTML = "NDEF message read.";
-            delay(400).then(() => color.style.backgroundColor = "#00ff00");
             delay(400).then(() => startScanning());
         });
 
+        // If it get a error while starting the scan
         }).catch((error) => {
         text.innerHTML = `Error! Scan failed to start: ${error}.`;
         color.style.backgroundColor = "#ff0000";
@@ -34,40 +37,33 @@ function startScanning(){
     
 }
 
-//Look if the device have NFC
+// Slut på koden som ska till service worker
+
+
+// Look if the device have NFC
 if ('NDEFReader' in window) {
     text.innerHTML = "Look if the device have NFC";
 
     color.style.backgroundColor = "#ffff00";
 
-
-    //https://developer.mozilla.org/en-US/docs/Web/API/WorkerNavigator/permissions
-    //https://whatwebcando.today/permissions.html
-    //https://developer.mozilla.org/en-US/docs/Web/API/Permissions/query
-
-    //Look if have permissions for a nfc is granted or not if not 
+    // Look if have permissions for a nfc is granted or not if permissions is not granded make a button that give browser permissions for nfc
     navigator.permissions.query({name:'nfc'}).then((result) => {
         if (result.state === 'granted') {
           startScanning();
         } else if (result.state === 'prompt') {
-            // Show a "scan" button.
+            // Show a scan button.
             document.querySelector("#scanButton").style.display = "block";
             document.querySelector("#scanButton").onclick = event => {
-            // Prompt user to allow UA to send and receive info when they tap NFC devices.
-            nfcText.innerHTML = result.state;
+            // Prompt user to allow to send and receive info when they tap NFC devices.
+            document.querySelector("#scanButton").style.display = "none";
             startScanning();
-          
-          
             };
         }
       });
     
-    //Start scaning for NFC tags
-
-    nfcText.innerHTML = navigator.permissions.query({name:'nfc'}).state;
-    
 }
 else{
+    // If device have no nfc reader or browser does not support NDEFReader
     text.innerHTML = "No nfc reader or browser does not support NDEFReader";
     color.style.backgroundColor = "#0000ff";
 }
