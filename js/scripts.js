0f8dff7d1213d38cc09481e0b12deb2821964221
williamhansen03 <william.hansen03@skola.stenungsund.se>
2022-11-30 18:42:19 +0100

    Write till NFC tagg

--- js/scripts.js
+++ js/scripts.js
@@ -19,25 +19,44 @@ function startScanning(){
         ndef.addEventListener("readingerror", () => {
             text.innerHTML = "Error! Cannot read data from the NFC tag. Try a different one?";
             color.style.backgroundColor = "#ff0000";
-            delay(400).then(() => startScanning());
+            delay(1000).then(() => startScanning());
         });
         // If you reading a tag successful 
         ndef.addEventListener("reading", ({ message, serialNumber }) => {
             info.innerHTML = message + ", " + serialNumber;
             text.innerHTML = "NDEF message read.";
-            delay(400).then(() => startScanning());
+            delay(1000).then(() => startScanning());
         });
 
         // If it get a error while starting the scan
         }).catch((error) => {
         text.innerHTML = `Error! Scan failed to start: ${error}.`;
         color.style.backgroundColor = "#ff0000";
-        delay(400).then(() => startScanning());
+        delay(1000).then(() => startScanning());
     });
     
 }
 
 // Slut på koden som ska till service worker
+
+const inputButton = document.querySelector(".inputButton");
+
+function inputFunction(){
+    const input = document.querySelector("#write");
+    const p = document.querySelector("p");
+    color.style.backgroundColor = "white";
+    text.style.color = "black";
+    info.style.color = "black";
+
+    if(input.value === "write"){
+        p.innerHTML = "write";
+        writeFunction();
+    }
+    else if (input.value === "scan"){
+        p.innerHTML = "scan";
+        startScanning();
+    }
+}
 
 
 // Look if the device have NFC
@@ -49,14 +68,19 @@ if ('NDEFReader' in window) {
     // Look if have permissions for a nfc is granted or not if permissions is not granded make a button that give browser permissions for nfc
     navigator.permissions.query({name:'nfc'}).then((result) => {
         if (result.state === 'granted') {
-          startScanning();
+            color.style.backgroundColor = "hotpink";
+            inputButton.addEventListener(`click`, inputFunction);
+          
         } else if (result.state === 'prompt') {
             // Show a scan button.
             document.querySelector("#scanButton").style.display = "block";
             document.querySelector("#scanButton").onclick = event => {
             // Prompt user to allow to send and receive info when they tap NFC devices.
-            document.querySelector("#scanButton").style.display = "none";
-            startScanning();
+                document.querySelector("#scanButton").style.display = "none";
+                color.style.backgroundColor = "black";
+                text.style.color = "white";
+                info.style.color = "whtie";
+                inputButton.addEventListener(`click`, inputFunction);
             };
         }
       });
@@ -70,4 +94,16 @@ else{
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
+}
+
+function writeFunction(){
+    const ndef = new NDEFReader();
+
+    ndef.write("1").then(() => {
+        text.innerHTML = "Message written.";
+
+    }).catch(error => {
+        text.innerHTML = `Write failed :-( try again: ${error}.`;
+
+    });
 }
